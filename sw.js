e58c8f94a7b481805504f5952eaecdf10f081fac
kevin79000 <guellec.coachingpro@gmail.com>
2026-05-19 19:23:21 +0200

    Switch to New Rocker gothic font

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'repcore-v14';
+const CACHE = 'repcore-v15';
 const FILES = ['./index.html', './manifest.json', './icons/icon-192x192.png', './icons/icon-512x512.png'];
 
 self.addEventListener('install', e => {
